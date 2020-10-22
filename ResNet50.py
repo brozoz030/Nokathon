ef04d0d9ef585567b45327ea4e221a1a5af013f4
brozoz030 <73314575+brozoz030@users.noreply.github.com>
2020-10-23 00:25:00 +0300

    Update ResNet50.py

--- ResNet50.py
+++ ResNet50.py
@@ -1,9 +1,16 @@
+import torch
+import torch.nn as nn
 import torch as T
 import torch.nn as nn
 import torchvision
 from torchvision import datasets, transforms
 import torch.nn.functional as F
 import numpy as np
+
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 
 def load_data(data_folder, batch_size, train):
@@ -37,13 +44,15 @@ learning_rate = 0.001
 
 dataset = load_data('/home/mi3/Desktop/Nokathon/', batch_size, 'train')
 
-def conv1x1(in_channels,out_channels, stride=1):
-    return nn.Conv2d(in_channels,out_channels,kernel_size=1,stride=stride,bias = False)        
 
 
-def conv3x3(in_channels,out_channels, stride=1,groups = 1, dilation=1):
-    return nn.Conv2d(in_channels,out_channels,kernel_size=3,stride=stride,padding=dilation, groups=groups,bias = False, dilation=dilation)
+__all__ = ['ResNet', 'resnet50']
 
+
+model_urls = {
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    
+}
 
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
@@ -98,11 +107,7 @@ class BasicBlock(nn.Module):
 
 
 class Bottleneck(nn.Module):
-    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
-    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
-    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
-    # This variant is also known as ResNet V1.5 and improves accuracy according to
-    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
 
     expansion = 4
 
@@ -130,7 +135,7 @@ class Bottleneck(nn.Module):
         out = self.bn1(out)
         out = self.relu(out)
 
-        out = self.conv2(out,out,out)
+        out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
 
@@ -172,7 +177,7 @@ class ResNet(nn.Module):
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0], )
+        self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
                                        dilate=replace_stride_with_dilation[0])
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
@@ -180,7 +185,7 @@ class ResNet(nn.Module):
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * 4, num_classes)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -189,9 +194,6 @@ class ResNet(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
         if zero_init_residual:
             for m in self.modules():
                 if isinstance(m, Bottleneck):
@@ -206,18 +208,18 @@ class ResNet(nn.Module):
         if dilate:
             self.dilation *= stride
             stride = 1
-        if stride != 1 or self.inplanes != planes * 4:
+        if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * 4, stride),
-                norm_layer(planes * 4),
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
             )
-        #block = (self.inplanes, planes, stride, self.groups,self.base_width, previous_dilation, norm_layer)
+
         layers = []
-        layers.append(Bottleneck(self.inplanes, planes, stride, self.groups,
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
                             self.base_width, previous_dilation, norm_layer))
-        self.inplanes = planes * 4
+        self.inplanes = planes * block.expansion
         for _ in range(1, blocks):
-            layers.append(Bottleneck(self.inplanes, planes, groups=self.groups,
+            layers.append(block(self.inplanes, planes, groups=self.groups,
                                 base_width=self.base_width, dilation=self.dilation,
                                 norm_layer=norm_layer))
 
@@ -236,7 +238,7 @@ class ResNet(nn.Module):
         x = self.layer4(x)
 
         x = self.avgpool(x)
-        x = T.flatten(x, 1)
+        x = torch.flatten(x, 1)
         x = self.fc(x)
 
         return x
@@ -244,15 +246,22 @@ class ResNet(nn.Module):
     def forward(self, x):
         return self._forward_impl(x)
 
-def _resnet(arch, block, layers, BasicBlock, **kwargs):
-    model = ResNet(block, layers, **kwargs)
-    
-    return model    
-        
 
-def resnet50(Bottleneck , **kwargs):
-   
-    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3],BasicBlock,
+def _resnet(arch, block, layers, pretrained, progress, **kwargs):
+    model = ResNet(block, layers, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+
+
+
+
+def resnet50(pretrained=False, progress=True, **kwargs):
+    
+    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress,
                    **kwargs)
 
 
